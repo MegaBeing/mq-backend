e825e206e1f5d5a6e6148655f77f6331def24030
Sahil <sahil@coastr.com>
2024-11-13 16:08:29 +0530

    removed graceful shutdown and linting

--- src/Models/Client.ts
+++ src/Models/Client.ts
@@ -1,35 +1,30 @@
 // Client Connection
 const { MongoClient, ServerApiVersion } = require('mongodb');
 export class MongoDBClient {
-    client : any
+    client: any
     async create_connection() {
-        try{
-        const URI = process.env.MONGODB_URI
-        this.client = new MongoClient(URI, {
-            serverApi: {
-                version: ServerApiVersion.v1,
-                strict: true,
-                deprecationErrors: true,
+        try {
+            const URI = process.env.MONGODB_URI
+            this.client = new MongoClient(URI, {
+                serverApi: {
+                    version: ServerApiVersion.v1,
+                    strict: true,
+                    deprecationErrors: true,
+                }
             }
+            )
+            await this.client.connect();
+            console.log(`MongoDB Server Connected 👍`)
+        } catch (error) {
+            throw error;
         }
-        )
-        await this.client.connect();
-        console.log(`MongoDB Server Connected 👍`)
-    }catch(error){
-        throw error;
     }
-    }
+
     async get_db(db: string) {
-        
         return await this.client.db(db)
     }
     async close_connection() {
         await this.client.close();
         console.log("MongoDB client closed. Exiting process.");
-    }
-    async gracefulShutdown(signal: string) {
-        console.log(`Received ${signal}. Closing MongoDB client.`);
-        await this.close_connection();
-        process.exit(0);
     }
 }
